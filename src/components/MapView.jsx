74dbc0d303d733d12a3932694aea8881abbee717
Kaustubh <kaustubh@truestate.in>
2023-12-20 00:09:30 +0530

    markers update

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -9,11 +9,21 @@ import MultipleSelect from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import villaPre from "../assets/Icon/Villa normal pre launch.svg";
+import villaNormal from "../assets/Icon/Villa normal.svg";
+import rowHousePre from "../assets/Icon/Row House Pre launch.svg";
+import rowHouseNormal from "../assets/Icon/Row House Normal.svg";
+import landPre from "../assets/Icon/Land Normal pre launch.svg";
+import landNormal from "../assets/Icon/Land Normal.svg";
+import apartmentPre from "../assets/Icon/Apartment Normal Pre launch.svg";
+import apartmentNormal from "../assets/Icon/Apartment Normal.svg";
 
+/*
 const customIcon = new Icon({
   iconUrl: markerImage,
   iconSize: [30, 30],
 });
+*/
 
 const typeOptions = [
   { value: "All", label: "All" },
@@ -27,7 +37,7 @@ const availabilityOptions = [
   { value: "All", label: "All" },
   { value: "Available", label: "Available" },
   { value: "Sold Out", label: "Sold Out" },
-  { value: "Pre-Launch", label: "Pre-Launch" },
+  { value: "Pre Launch", label: "Pre Launch" },
 ];
 
 const areaOptions = [
@@ -133,6 +143,25 @@ const micromarketOptions = [
   "Yamare",
 ];
 
+const getMarkerIcon = (typeOptions, availabilityOptions) => {
+  switch (typeOptions) {
+    case "Villa":
+      return availabilityOptions === "Pre Launch" ? villaPre : villaNormal;
+    case "Rowhouse":
+      return availabilityOptions === "Pre Launch"
+        ? rowHousePre
+        : rowHouseNormal;
+    case "Land":
+      return availabilityOptions === "Pre Launch" ? landPre : landNormal;
+    case "Apartment":
+      return availabilityOptions === "Pre Launch"
+        ? apartmentPre
+        : apartmentNormal;
+    default:
+      return markerImage;
+  }
+};
+
 const MapView = ({ properties }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredProperties, setFilteredProperties] = useState(properties);
@@ -174,15 +203,15 @@ const MapView = ({ properties }) => {
   }, [searchQuery, selectedTypes, selectedAvailability, selectedAreas]);
 
   return (
-    <div className="flex flex-col justify-between max-w-[84rem] mx-auto text-center px-6 py-3 mt-2">
-      <div className="flex sm:flex-col pr:flex-col lg:space-x-10 ld:space-x-8 mb-6 w-full sm:space-y-4 pr:space-y-4">
+    <div className="flex flex-col justify-between text-center mt-2">
+      <div className="flex sm:flex-col pr:flex-col lg:space-x-10 ld:space-x-8 mb-2 w-full sm:space-y-4 pr:space-y-4 max-w-[84rem] mx-auto px-3 py-3">
         <div className="lg:w-[36%] ld:w-[40%] pr:w-full sm:w-full">
           <input
             type="text"
             placeholder="Search by property name or micromarket"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            className="border border-black rounded-2xl w-full h-full lg:px-4 ld:px-4 pr:px-4 pr:py-4 sm:px-4 sm:py-4"
+            className="border-[#D1D1D1] border-2 rounded-xl w-full h-full lg:px-4 ld:px-4 pr:px-4 pr:py-4 sm:px-4 sm:py-4"
           />
         </div>
         <div className="flex lg:w-full ld:w-[60%] pr:w-full sm:w-full lg:space-x-20 ld:space-x-6 pr:space-x-10 sm:space-x-6 lg:justify-between ld:justify-between pr:justify-between sm:justify-between">
@@ -249,7 +278,7 @@ const MapView = ({ properties }) => {
             </MultipleSelect>
           </FormControl>
           <button
-            className="bg-red-700 hover:bg-red-900 text-white lg:text-sm ld:text-sm pr:text-sm sm:text-sm lg:font-bold ld:font-bold pr:font-bold sm:font-bold lg:px-8 ld:px-4 pr:px-6 sm:px-4 rounded-full"
+            className="text-[#F4744C] border border-[#F4744C] lg:text-base ld:text-base pr:text-base sm:text-base lg:font-bold ld:font-bold pr:font-bold sm:font-bold lg:px-8 ld:px-4 pr:px-6 sm:px-4 rounded-full w-[11rem] justify-center"
             onClick={() => {
               setSearchQuery("");
               setSelectedTypes(["All"]);
@@ -257,7 +286,7 @@ const MapView = ({ properties }) => {
               setSelectedAreas(["All"]);
             }}
           >
-            Clear
+            Clear Filters
           </button>
         </div>
       </div>
@@ -273,47 +302,54 @@ const MapView = ({ properties }) => {
           maxZoom={20}
           subdomains={["mt0", "mt1", "mt2", "mt3"]}
         />
-        {filteredProperties.map((property) => (
-          <Marker
-            key={property.id}
-            position={property.geocode}
-            icon={customIcon}
-          >
-            <Popup className="w-auto">
-              <div>
-                <strong>Name:- {property.popUp}</strong>
-              </div>
-              <div>
-                <strong>Rera Id:- </strong>
-                {property.rera_id}
-              </div>
-              <div>
-                <strong>Type:- </strong>
-                {property.asset_type}
-              </div>
-              <div>
-                <strong>Availability:- </strong>
-                {property.availability}
-              </div>
-              <div>
-                <strong>Price in Sq/feet:- </strong>
-                {property.price_Sq}
-              </div>
-              <div>
-                <strong>Area:- </strong>
-                {property.area}
-              </div>
-              <div>
-                <strong>Micromarket:- </strong>
-                {property.micromarket}
-              </div>
-              <div>
-                <strong>Developer:- </strong>
-                {property.developer}
-              </div>
-            </Popup>
-          </Marker>
-        ))}
+        {filteredProperties.map((property) => {
+          const markerIcon = new Icon({
+            iconUrl: getMarkerIcon(property.asset_type, property.availability),
+            iconSize: [30, 30],
+          });
+
+          return (
+            <Marker
+              key={property.id}
+              position={property.geocode}
+              icon={markerIcon}
+            >
+              <Popup className="w-auto">
+                <div>
+                  <strong>Name:- {property.popUp}</strong>
+                </div>
+                <div>
+                  <strong>Rera Id:- </strong>
+                  {property.rera_id}
+                </div>
+                <div>
+                  <strong>Type:- </strong>
+                  {property.asset_type}
+                </div>
+                <div>
+                  <strong>Availability:- </strong>
+                  {property.availability}
+                </div>
+                <div>
+                  <strong>Price in Sq/feet:- </strong>
+                  {property.price_Sq}
+                </div>
+                <div>
+                  <strong>Area:- </strong>
+                  {property.area}
+                </div>
+                <div>
+                  <strong>Micromarket:- </strong>
+                  {property.micromarket}
+                </div>
+                <div>
+                  <strong>Developer:- </strong>
+                  {property.developer}
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
